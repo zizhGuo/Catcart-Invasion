563e57cb5f04ef5386c1e9ca96ebfa49ebcd09db
Zizhun Guo <zg2808@g.rit.edu>
2018-05-22 10:25:49 -0400

    [UI] Start Menu Buttons.

--- Assets/Non_VR_Project/Scripts/Menu/MainMenu.cs
+++ Assets/Non_VR_Project/Scripts/Menu/MainMenu.cs
@@ -12,6 +12,13 @@ public class MainMenu : MonoBehaviour
     public GameObject MainMenu1;
     public GameObject PlayMenu;
     public GameObject SettingMenu;
+    public GameObject QuitMenu;
+    public GameObject Cat_1;
+    public GameObject Cat_2;
+    public GameObject Cat_3;
+    public GameObject Cat_4;
+    public GameObject Cat_5;
+    public GameObject Arrow;
 
 
     private void Awake()
@@ -21,6 +28,10 @@ public class MainMenu : MonoBehaviour
     //public void PlayerGame() {
     //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     //}
+    
+    /// <summary>
+    /// MainMenu
+    /// </summary>
 
     public void OnMouseClickPlay()
     {
@@ -29,6 +40,7 @@ public class MainMenu : MonoBehaviour
         MainMenu1.SetActive(false);
         PlayMenu.SetActive(true);
         SettingMenu.SetActive(false);
+        QuitMenu.SetActive(false);
     }
     public void OnMouseOverPlay()
     {
@@ -40,7 +52,15 @@ public class MainMenu : MonoBehaviour
         CameraInstance.instance.transform.DOMove(Position, 0.5f).SetEase(Ease.InOutQuad);
         CameraInstance.instance.transform.DORotate(Rotation, 0.5f).SetEase(Ease.InOutQuad);
     }
-
+    public void OnMouseClickSettiing()
+    {
+        CameraInstance.instance.transform.position = new Vector3(6.479673f, 2.458701f, 9.382029f);
+        CameraInstance.instance.transform.rotation = Quaternion.Euler(new Vector3(0.208f, 0.306f, 0));
+        MainMenu1.SetActive(false);
+        PlayMenu.SetActive(false);
+        SettingMenu.SetActive(true);
+        QuitMenu.SetActive(false);
+    }
     public void OnMouseOverSetting()
     {
         CameraInstance.instance.transform.DOMove(new Vector3(6.479673f, 2.458701f, 9.382029f), 0.5f).SetEase(Ease.InOutQuad);
@@ -52,7 +72,15 @@ public class MainMenu : MonoBehaviour
         CameraInstance.instance.transform.DORotate(Rotation, 0.5f).SetEase(Ease.InOutQuad);
     }
 
-
+    public void OnMouseClickQuit()
+    {
+        CameraInstance.instance.transform.position = new Vector3(11.41322f, 4.000476f, 8.336395f);
+        CameraInstance.instance.transform.rotation = Quaternion.Euler(new Vector3(-0.823f, 23.511f, 0));
+        MainMenu1.SetActive(false);
+        PlayMenu.SetActive(false);
+        SettingMenu.SetActive(false);
+        QuitMenu.SetActive(true);
+    }
     public void OnMouseOverQuit()
     {
         CameraInstance.instance.transform.DOMove(new Vector3(11.41322f, 4.000476f, 8.336395f), 0.5f).SetEase(Ease.InOutQuad);
@@ -64,6 +92,73 @@ public class MainMenu : MonoBehaviour
         CameraInstance.instance.transform.DORotate(Rotation, 0.5f).SetEase(Ease.InOutQuad);
     }
 
+    /// <summary>
+    /// PlayMenu
+    /// </summary>
+    public void OnMouseClickHost()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void OnMouseOverHost()
+    {
+        Cat_1.transform.DOShakeScale(0.5f, 0.01f, 10, 90);
+    }
+    public void OnMouseClickJoin()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OnMouseOverJoin()
+    {
+        Cat_2.transform.DOShakeScale(0.5f, 0.3f, 10, 90);
+    }
+    public void OnMouseClickBack()
+    {
+        CameraInstance.instance.transform.DOMove(Position, 0.5f).SetEase(Ease.InOutQuad);
+        CameraInstance.instance.transform.DORotate(Rotation, 0.5f).SetEase(Ease.InOutQuad);
+        MainMenu1.SetActive(true);
+        PlayMenu.SetActive(false);
+        SettingMenu.SetActive(false);
+        QuitMenu.SetActive(false);
+    }
+    public void OnMouseOverBack_main()
+    {
+        Cat_3.transform.DOShakeScale(0.5f, 0.3f, 10, 90);
+    }
+
+    /// <summary>
+    /// Setting Menu
+    /// </summary>
+    public void OnMouseChangedTutorial()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        Cat_4.transform.DOShakeScale(0.5f, 0.3f, 10, 90);
+    }
+    public void OnMouseOverTutoria()
+    {
+        Cat_4.transform.DOShakeScale(0.5f, 0.3f, 10, 90);
+    }
+    public void OnMouseOverBack_setting()
+    {
+        Cat_4.transform.DOShakeScale(0.5f, 0.3f, 10, 90);
+    }
+
+    /// <summary>
+    /// Quit Menu
+    /// </summary>
+    /// 
+    public void OnMouseClickYes_quit()
+    {
+        Application.Quit();
+    }
+    public void OnMouseOverYes_quit()
+    {
+        Arrow.transform.DOShakeScale(0.5f, 3f, 10, 90);
+    }
+
+    public void OnMouseOverNo_quit()
+    {
+        Cat_5.transform.DOShakeScale(0.5f, 0.3f, 10, 90);
+    }
     private void Update()
     {
         if (Input.GetKey(KeyCode.A)) CameraInstance.instance.transform.position = new Vector3(0, 0, 0);
